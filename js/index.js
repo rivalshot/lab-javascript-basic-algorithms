74a79ca971bd2f1b46753ff069206e974d6d8c7a
rivalshot <76807186+rivalshot@users.noreply.github.com>
2021-01-16 13:33:53 +0400

    work done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
-// Iteration 1: Names and Input
+let hacker1 = 'Weronika'
+console.log(`driver's name is ${hacker1}`)
+
+let hacker2 = 'Hugo'
+console.log(`navigator's name is ${hacker2}`)
+
+console.log(`The driver has the longest name, it has ${hacker1.length} characters`)
 
 
-// Iteration 2: Conditionals
+for (let n of hacker1) {
+    console.log(n.toUpperCase());
+}
 
+for (let number = hacker2.length - 1; number > -1; number--) {
+  console.log(hacker2[number]);
+}
 
-// Iteration 3: Loops
+if (hacker1 > hacker2) {
+  console.log("The driver's name goes first.");
+} else if (hacker2 > hacker1) {
+  console.log(
+    "Yo, the navigator goes first definitely."
+  );
+}
